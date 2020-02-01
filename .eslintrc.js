7475adc9d00f77f52d07bacb25797c1a1df322d0
Diego <diego.fernandez@tiendeo.com>
2020-02-01 14:08:43 +0100

    Initial create react app commit, add Prettier, eslint, remove default files

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    jest: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'prettier',
+    'prettier/react',
+    'prettier/standard',
+    'plugin:jsx-a11y/recommended',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: ['react', 'react-hooks', 'import', 'prettier', 'react'],
+  rules: {
+    'prettier/prettier': 'error',
+  },
+};
