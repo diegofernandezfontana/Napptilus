e3e1bc5a6bf2ba56bb09978db54213d2ceb0b663
Diego <diego.fernandez@tiendeo.com>
2020-02-02 22:15:32 +0100

    Add ompaloompa profile initial mockup and logic

--- src/Pages/UmpaLumpaProfile/index.js
+++ src/Pages/UmpaLumpaProfile/index.js
@@ -1,6 +1,76 @@
-import React from 'react';
+import React, { useEffect, Fragment } from 'react';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+import { withRouter } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
-const UmpaLumpaProfile = () => {
-  return <h1>UmpaLumpa Profile</h1>;
+import { selectSelectedOompaLoompa, selectIsLoadingInit } from '../Main/selectors';
+
+import { Wrapper, ProfileImage, ProfileTitle } from './styles';
+import { getSelectedOompaLoompaById } from '../Main/actions';
+
+const UmpaLumpaProfile = props => {
+  const { selectedOompaLoompa, onGetSelectedOompaLoompaById, isLoadingInit } = props;
+
+  useEffect(() => {
+    if (!isLoadingInit && !Object.keys(selectedOompaLoompa).length) {
+      const {
+        match: {
+          params: { id },
+        },
+      } = props;
+
+      onGetSelectedOompaLoompaById({ id });
+    }
+  }, [isLoadingInit]);
+
+  const renderProfileInfo = () => {
+    if (!isLoadingInit && Object.keys(selectedOompaLoompa).length) {
+      const { first_name, last_name, gender, profession } = selectedOompaLoompa;
+
+      return (
+        <Fragment>
+          <h1>{first_name}</h1>
+          <h1>{last_name}</h1>
+          <h1>{gender}</h1>
+          <h1>{profession}</h1>
+        </Fragment>
+      );
+    }
+
+    return null;
+  };
+
+  const renderProfile = () => {
+    if (isLoadingInit) {
+      return <h1> Loading....</h1>;
+    }
+
+    return (
+      <Wrapper>
+        <ProfileImage />
+        <ProfileTitle>{renderProfileInfo()}</ProfileTitle>
+      </Wrapper>
+    );
+  };
+
+  return renderProfile();
 };
-export default UmpaLumpaProfile;
+
+const mapDispatchToProps = dispatch => ({
+  onGetSelectedOompaLoompaById: ({ id }) => {
+    dispatch(getSelectedOompaLoompaById({ id }));
+  },
+});
+
+const mapStateToProps = createStructuredSelector({
+  selectedOompaLoompa: selectSelectedOompaLoompa(),
+  isLoadingInit: selectIsLoadingInit(),
+});
+
+UmpaLumpaProfile.propTypes = {
+  selectedOompaLoompa: PropTypes.object,
+  onGetSelectedOompaLoompaById: PropTypes.func,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(withRouter(UmpaLumpaProfile));
---
+++ src/Pages/UmpaLumpaProfile/styles.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+  border: 2px solid red;
+  height: 250px;
+  width: 300px;
+  box-sizing: border-box;
+`;
+
+export const ProfileImage = styled.div`
+  width: 100%;
+  height: 100px;
+  border: 5px solid black;
+`;
+
+export const ProfileTitle = styled.div`
+  color: red;
+`;
